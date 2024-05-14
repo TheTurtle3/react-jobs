b162e229425ed99c5783892345e631148228deb1
Ahmad Zafar <amz9616@gmail.com>
2024-05-14 13:15:23 -0400

    Data is now retrieved through an api

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,14 @@ import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import JobPage from "./pages/JobPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" index element={<JobsPage />} />
+      <Route path="/job/:id" index element={<JobPage />} />
       <Route path="*" index element={<NotFoundPage />} />
     </Route>
   )
